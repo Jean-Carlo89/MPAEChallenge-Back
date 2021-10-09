d7679158718aeb92cc951f4cc6017e08e7cf7504
Jean Carlo <jeancarlomo@hotmail.com>
2021-10-09 17:55:51 -0300

    changing origin

--- src/app.js
+++ src/app.js
@@ -4,8 +4,9 @@ import axios from "axios";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({origin:"*"}));
 app.use(express.json());
+
 app.use(function(req,res,next){
   res.header("Access-Control-Allow-Origin","https://mpaec-hallenge.vercel.app/")
   res.header("Access-Control-Allow-Origin","Origin,X-Requested-With,Content-Type,Accept")
